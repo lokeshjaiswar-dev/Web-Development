07e387bb9fe94137c2ff1d84802d35c5d7f1a8c1
Lokesh Jaiswar <lokeshjaiswar.dev@gmail.com>
2026-06-27 23:16:29 +0530

    18_REFERENCE_TYPES

---
+++ JS/18_REFERENCE_TYPES/srcipt.js
@@ -0,0 +1,44 @@
+const name = "Lokesh"
+const username = ""
+const username1 = ""
+const user = {
+    firstName: "Pooja",
+    lastName: "Jaiswar"
+}
+
+// const user2 = {
+//     firstName: "Lokesh",
+//     'last-Name': "Jaiswar",
+//     Lokesh: "Developer"
+// }
+
+// to access properties of object
+// console.log(user2.firstName);
+// console.log(user2["last-Name"]);
+// console.log(user2.name)
+// console.log(user2[name]);
+// console.log(user2['first'+'Name'])
+
+const user2 = {
+    firstName: "Lokesh",
+    lastName: "Jaiswar",
+    pata: {
+        city: "Mumbai",
+        state: 'Maharashtra',
+        pincode: 400086,
+        moreDetails: {
+            population: 23424234,
+            area: '233 sq km'
+        }
+    }
+}
+
+// to add properties in object
+// user2.age = 21
+// user2['gender'] = "Male"
+// console.log(user2);
+
+
+
+
+
