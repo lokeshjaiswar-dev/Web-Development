35b7f723cf196d588a86a85e5efb941f3348d0d2
Lokesh Jaiswar <lokeshjaiswar.dev@gmail.com>
2026-06-29 02:27:47 +0530

    22 COPY IN JS

---
+++ JS/22_SHALLOW&DEEP_COPY/script.js
@@ -0,0 +1,29 @@
+// const fruits = ['apple', 'banana', 'chickoee', 'mango']
+
+// const myFruits = fruits;
+
+// myFruits.push('orange')
+// myFruits.push('guava')
+
+const user1 = {
+    firstName: "Lokesh",
+    lastName: "Jaiswar",
+    address: {
+        city: "Mumbai",
+        pincode: "400086"
+    }
+}
+const user2 = JSON.parse(JSON.stringify(user1))
+// const user2 = { ...user1 }
+
+// const user2 = user1;
+
+// user2.lastName = "Trump"
+
+// let name1 = 'Lokesh'
+// let name2 = name1;
+// console.log(name2)
+
+// name2 = 'Jayesh'
+// console.log(name2)
+// console.log(name1)
