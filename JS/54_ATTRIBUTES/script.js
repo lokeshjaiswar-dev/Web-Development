d2dd66510cbeb782585d2320de1f257808b5699e
Lokesh Jaiswar <lokeshjaiswar.dev@gmail.com>
2026-06-30 17:03:49 +0530

    54_ATTRIBUTES

--- JS/54_ATTRIBUTES/script.js
+++ JS/54_ATTRIBUTES/script.js
@@ -1,2 +1,17 @@
 const allJaiswar = document.querySelectorAll('[jaiswar]')
 const Lokesh = document.querySelector('[jaiswar="Lokesh"]')
+
+// accessing the attribute without getAttribute
+console.log(Lokesh.attributes.jaiswar.value)
+
+// Setting the attribute without setAttribute
+Lokesh.attributes.jaiswar.value = 'Pooja'
+
+// after changing the attribute, then changing the content of element
+document.querySelector('[jaiswar="Pooja"]').innerText = 'My name is Pooja'
+
+// now using the method given by the dom
+document.querySelector('[jaiswar="Pooja"]').getAttribute('jaiswar')
+
+// now setting using the method of dom
+document.querySelector('[jaiswar="Pooja"]').setAttribute('title', 'Hello World')
