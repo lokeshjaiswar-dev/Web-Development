c3efbe199a1de0180630c0e9731d1aa5b3c0186e
Lokesh Jaiswar <lokeshjaiswar.dev@gmail.com>
2026-07-02 01:53:15 +0530

    61_EVENTS

--- JS/61_EVENTS/script.js
+++ JS/61_EVENTS/script.js
@@ -1,7 +1,31 @@
 const h1 = document.querySelector('h1')
+const container = document.querySelector('.container')
+const card = document.querySelector('.card')
 
-function sayHi(){
-    console.log("hiiiiiii")
-}
+let counter = 1
 
-h1.onclick = sayHi
+card.addEventListener('click', function(){
+    const div = document.createElement('div')
+    div.classList.add('card')
+    div.textContent = counter++
+    container.append(div)
+})
+
+
+
+
+
+// function sayHi(){
+//     console.log("hiiiiiii")
+// }
+
+// h1.onclick = sayHi
+// h1.onclick = function(){
+//     console.log("lokesh")
+// }
+
+// h1.addEventListener('click', function(){
+//     console.log("Lokesh")
+// })
+
+// h1.addEventListener('click', sayHi)
