dc1b5aea1abcaa501a9f3cda63a6eafdc93f10dd
Lokesh Jaiswar <lokeshjaiswar.dev@gmail.com>
2026-07-04 23:08:16 +0530

    62_EVENTOBJECT

---
+++ JS/62_EVENTOBJECT/script.js
@@ -0,0 +1,40 @@
+const usernameInput = document.querySelector('#username')
+
+// usernameInput.addEventListener('click', function(){
+//     console.log('clicked')
+// })
+
+// usernameInput.addEventListener('dblclick', function(e){
+//     console.log(e);
+//     console.log('double clicked')
+// })
+
+// usernameInput.addEventListener('input', function(e){
+//     // console.log("Data",e.data);
+//     console.log('Value',e.target.value)
+//     // console.log('input event triggered')
+// })
+
+// usernameInput.addEventListener('change', function(e){
+    
+//     console.log('Value',e.target.value)
+
+// })
+
+// usernameInput.addEventListener('focus', function(e){
+
+//     console.log('focus',e.target.value)
+
+// })
+
+// usernameInput.addEventListener('blur', function(e){
+
+//     console.log('blur',e.target.value)
+
+// })
+
+const form = document.querySelector('form');
+form.addEventListener('submit',function(e){
+    e.preventDefault()
+    console.log('konsa bhi event laga sakte hai par mainly submit event lagate hai')
+})
