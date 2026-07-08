6f7ee3228f09c9cfc4d6b38982cce42d4b58917e
Lokesh Jaiswar <lokeshjaiswar.dev@gmail.com>
2026-07-08 22:24:03 +0530

    62_EVENTOBJECT

--- JS/62_EVENTOBJECT/script.js
+++ JS/62_EVENTOBJECT/script.js
@@ -38,8 +38,7 @@ const usernameInput = document.querySelector('#username')
 const form = document.querySelector('form');
 form.addEventListener('submit',function(e){
 
-    // iska notes likhna hai
-    // e.preventDefault() 
+    e.preventDefault() 
     console.log('konsa bhi event laga sakte hai par mainly submit event lagate hai')
 })
 
